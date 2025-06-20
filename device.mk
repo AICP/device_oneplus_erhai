4c4fefcdd709d2cae843a276d5ceb3edfd4c598d
Bruno Martins <bgcngm@gmail.com>
2025-06-20 18:54:18 +0100

    erhai: Setup prop overrides for Pad 2 Pro and Pad 3
    Change-Id: I6a8544a9284ed7fc5b3b6b98a68f84fbffbf6036

--- device.mk
+++ device.mk
@@ -34,6 +34,11 @@ PRODUCT_PACKAGES += \
     OPlusFrameworksResTarget \
     OPlusSystemUIResTarget
 
+# Regional properties
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/recovery/root/vendor/odm/etc/24926/build.default.prop:$(TARGET_COPY_OUT_ODM)/etc/24926/build.default.prop \
+    $(LOCAL_PATH)/recovery/root/vendor/odm/etc/24976/build.default.prop:$(TARGET_COPY_OUT_ODM)/etc/24976/build.default.prop
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
