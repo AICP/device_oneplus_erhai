d75dff5ddda937b9c85f514a2110ba4ecb70ba50
Bruno Martins <bgcngm@gmail.com>
2025-06-24 21:44:16 +0100

    erhai: audio: Bring in default volume configurations
    Change-Id: I078a155814087f37445231b4e11c29a3d489f8e6

--- device.mk
+++ device.mk
@@ -9,6 +9,11 @@ TARGET_IS_TABLET := true
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
+    $(LOCAL_PATH)/configs/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 3392
 TARGET_SCREEN_WIDTH := 2400
