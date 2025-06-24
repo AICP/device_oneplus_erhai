190d6c4ac19a77a8d36990e1793964a2d9a5f456
LuK1337 <priv.luk@gmail.com>
2025-06-25 00:59:09 +0200

    erhai: Set primary display orientation to 270
    Change-Id: I7e52e9586e8ee19e562facdb457acbba2c1c1661

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,14 +9,22 @@ BOARD_SUPER_PARTITION_SIZE := 11711471616
 # Include the common OEM chipset BoardConfig.
 include device/oneplus/sm8750-common/BoardConfigCommon.mk
 
+DEVICE_PATH := device/oneplus/erhai
+
 # Assert
 TARGET_OTA_ASSERT_DEVICE := OP615EL1,OP6190L1
 
 # Display
 TARGET_SCREEN_DENSITY := 420
 
+# Properties
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
 # Recovery
 TARGET_RECOVERY_DEFAULT_TOUCH_ROTATION := ROTATION_RIGHT
+
+# SEPolicy
+include $(DEVICE_PATH)/sepolicy/SEPolicy.mk
 
 # Include the proprietary files BoardConfig.
 include vendor/oneplus/erhai/BoardConfigVendor.mk
