27a5bd0cc1409012db1a8c5f95cbfd566ffe398f
LuK1337 <priv.luk@gmail.com>
2025-06-26 18:37:37 +0200

    erhai: Initial blobs list
    Change-Id: I804a0695eb4be2187decbceacc011fe2eaa49aa8

--- extract-files.py
+++ extract-files.py
@@ -4,19 +4,35 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixups,
+)
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
 )
 
 namespace_imports = [
+    'hardware/qcom-caf/sm8750',
     'vendor/oneplus/sm8750-common',
 ]
+
+blob_fixups: blob_fixups_user_type = {
+    'odm/bin/nvram_server': blob_fixup()
+        .binary_regex_replace(b'vendor.oplus.caihong.serialno', b'ro.boot.chipid' + 15 * b'\x00'),
+}  # fmt: skip
 
 module = ExtractUtilsModule(
     'erhai',
     'oneplus',
     namespace_imports=namespace_imports,
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    add_firmware_proprietary_file=True,
 )
 
 if __name__ == '__main__':
