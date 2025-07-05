af9d087f51d1becf639cd3d0b80edb15b6a9c56e
LuK1337 <priv.luk@gmail.com>
2025-07-06 00:57:00 +0200

    erhai: Set vendor.display.disable_idle_fps_switch=1
    Change-Id: I7fafa4cfc9ec35c3f07a43b17fd1119ec4ab5bc1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,6 +21,7 @@ TARGET_SCREEN_DENSITY := 420
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
 # Properties
+TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery
