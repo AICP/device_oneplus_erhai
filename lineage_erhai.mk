5e897c70e0346af268b4eb713fd46ae52e155a2b
LuK1337 <priv.luk@gmail.com>
2025-10-13 17:39:49 +0200

    erhai: Update from OOS 11.A.37
    Change-Id: I90aedbdcc818dec91d4fcae330e4966473d91ff1

--- lineage_erhai.mk
+++ lineage_erhai.mk
@@ -23,8 +23,8 @@ PRODUCT_CHARACTERISTICS := nosdcard,tablet
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="qssi_64-user 15 AP3A.240617.008 1754120502711 release-keys" \
-    BuildFingerprint=OnePlus/OPD2415EEA/OP6190L1:15/AP3A.240617.008/V.R4T3.1f022f1-11e3c-11e41:user/release-keys \
+    BuildDesc="qssi_64-user 15 AP3A.240617.008 1758608624134 release-keys" \
+    BuildFingerprint=OnePlus/OPD2415EEA/OP6190L1:15/AP3A.240617.008/V.R4T3.1ffacb0_1-12da3:user/release-keys \
     DeviceName=OP6190L1 \
     DeviceProduct=OPD2415 \
     SystemDevice=OP6190L1 \
