abbb18b08b9368b2e8c119ebd2d9a53e81763f2c
LuK1337 <priv.luk@gmail.com>
2025-10-14 16:22:30 +0200

    Revert "erhai: Build folio-daemon"
    Done kernel side now.
    
    This reverts commit 16b85d4cd8accdd13df467886fa389f2337d6fe3.
    
    Change-Id: Ib12a66418c9c4724b29dc08c220218317c168a14

--- device.mk
+++ device.mk
@@ -58,10 +58,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/root/vendor/odm/etc/24976/build.ROW.prop:$(TARGET_COPY_OUT_ODM)/etc/24976/build.ROW.prop \
     $(LOCAL_PATH)/recovery/root/vendor/odm/etc/24976/build.default.prop:$(TARGET_COPY_OUT_ODM)/etc/24976/build.default.prop
 
-# Sensors
-PRODUCT_PACKAGES += \
-    folio-daemon
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
