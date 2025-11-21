LineageOS/android_device_oneplus_erhai

erhai: Set pen sensitivity to 254 on boot
Change-Id: I8bcf5933c20a6791d136d388ec2b670639ebd51b

device.mk
@@ -29,6 +29,10 @@ WITH_GMS_COMMS_SUITE := false
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# Init
+PRODUCT_PACKAGES += \
+    init.device.rc
+
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/idc/Vendor_22d9_Product_3869.idc:$(TARGET_COPY_OUT_ODM)/usr/idc/Vendor_22d9_Product_3869.idc

AICP/device_oneplus_erhai

device.mk
@@ -29,6 +29,10 @@ WITH_GMS_COMMS_SUITE := false
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# Init
+PRODUCT_PACKAGES += \
+    init.erhai.rc
+
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/idc/Vendor_22d9_Product_3869.idc:$(TARGET_COPY_OUT_ODM)/usr/idc/Vendor_22d9_Product_3869.idc